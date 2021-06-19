2157320022ffaf60083f7b9b2eb5af12f42443a5
RaspiKidd <kerry@raspikidd.com>
2021-06-19 14:13:10 +0100

    EduKidd blog updated

--- pages/Blog.vue
+++ pages/Blog.vue
@@ -164,7 +164,7 @@
 
             <div class="relative max-w-7xl mx-auto">
                 <div class="text-center">
-                    <h1 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+                    <h1 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl text-red-600">
                         From the blog
                     </h1>
                     <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
@@ -183,7 +183,7 @@
                         :image="article.content.image.filename"
                         :date="article.content.date.toLocaleDateString()"
                     />
-                    <!--<div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                    <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
                         <div class="flex-shrink-0">
                             <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixqx=kMf6WlMMZH&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">
                         </div>
@@ -191,9 +191,7 @@
                         <div class="flex-1 bg-white p-6 flex flex-col justify-between">
                             <div class="flex-1">
                                 <p class="text-sm font-medium text-indigo-600">
-                                    <a href="#" class="hover:underline">
-                                        Article
-                                    </a>
+                                    How to
                                 </p>
                                 <a href="#" class="block mt-2">
                                     <p class="text-xl font-semibold text-gray-900">
@@ -235,7 +233,7 @@
                         </div>
                     </div>
 
-                    <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                    <!--<div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
                         <div class="flex-shrink-0">
                             <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=kMf6WlMMZH&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">
                         </div>
